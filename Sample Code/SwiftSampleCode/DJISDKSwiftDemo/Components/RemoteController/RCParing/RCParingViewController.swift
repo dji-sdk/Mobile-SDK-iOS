095ec72d5053d6ed1c0f1b9be6d226a9ef1fe0d4
Valeriy Van <github@w7software.com>
2016-05-06 09:46:54 +0300

    Makes RCParingViewController.swift more swifty.

--- Sample Code/SwiftSampleCode/DJISDKSwiftDemo/Components/RemoteController/RCParing/RCParingViewController.swift
+++ Sample Code/SwiftSampleCode/DJISDKSwiftDemo/Components/RemoteController/RCParing/RCParingViewController.swift
@@ -1,5 +1,5 @@
 //
-//  RCParingViewController.m
+//  RCParingViewController.swift
 //  DJISdkDemo
 //
 //  Created by DJI on 16/1/6.
@@ -10,27 +10,16 @@
 import DJISDK
 class RCParingViewController: DJIBaseViewController {
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view from its nib.
-    }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
     @IBAction func onStartParingButtonClicked(sender: AnyObject) {
-        let rc: DJIRemoteController? = self.fetchRemoteController()
-        if rc != nil {
-            rc!.enterRCToAircraftPairingModeWithCompletion({[weak self](error: NSError?) -> Void in
-                if error != nil {
-                    self?.showAlertResult("Start Failed: \(error!.localizedDescription)")
+        if let rc = self.fetchRemoteController() {
+            rc.enterRCToAircraftPairingModeWithCompletion{[weak self](error: NSError?) -> Void in
+                if let error = error {
+                    self?.showAlertResult("Start Failed: \(error.localizedDescription)")
                 }
                 else {
                     self?.showAlertResult("Start Succeeded.")
                 }
-            })
+            }
         }
         else {
             self.showAlertResult("Component Not Exist")
@@ -38,16 +27,15 @@ class RCParingViewController: DJIBaseViewController {
     }
     
     @IBAction func onStopParingButtonClicked(sender: AnyObject) {
-        let rc: DJIRemoteController? = self.fetchRemoteController()
-        if rc != nil {
-            rc!.exitRCToAircraftPairingModeWithCompletion({[weak self](error: NSError?) -> Void in
-                if error != nil {
-                    self?.showAlertResult("Stop Failed: \(error!.localizedDescription)")
+        if let rc = self.fetchRemoteController() {
+            rc.exitRCToAircraftPairingModeWithCompletion{[weak self](error: NSError?) -> Void in
+                if let error = error {
+                    self?.showAlertResult("Stop Failed: \(error.localizedDescription)")
                 }
                 else {
                     self?.showAlertResult("Stop Succeeded.")
                 }
-            })
+            }
         }
         else {
             self.showAlertResult("Component Not Exist")
